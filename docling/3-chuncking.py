cd8892aca159a2f7beffa1b469031c4a4b63a875
Pedro <pedromateusneto@gmail.com>
2026-03-05 11:19:08 -0300

    Criando primeiro código de chuncking

--- docling/3-chuncking.py
+++ docling/3-chuncking.py
@@ -1,12 +1,13 @@
 from docling.document_converter import DocumentConverter
+from docling.chunking import HierarchicalChunker
 
 converter = DocumentConverter()
 
-# result = converter.convert("./2408.09869v5.pdf")
-result = converter.convert("https://arxiv.org/pdf/2408.09869")
-
+result = converter.convert("./2408.09869v5.pdf")
 document = result.document
 
-markdown_output = document.export_to_markdown()
+chunker = HierarchicalChunker()
+chunks = list(chunker.chunk(document))
 
-print(markdown_output)
+chunks
+print(chunks[4].text)
